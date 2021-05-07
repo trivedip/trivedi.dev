3e27330e2efb0d3feb9aee9dd855ede8f9a19d23
ptrivedi <pri7ank@gmail.com>
2021-05-07 18:29:42 -0500

    accessibility improvements

--- src/components/indexComponents/latestBlogContainer.js
+++ src/components/indexComponents/latestBlogContainer.js
@@ -5,7 +5,7 @@ const LatestBlog = () =>{
     return(
         <>
         <section id="down" className="latest-blog-container mt-28">            
-            <div className="px-5 mb-3 font-medium uppercase tracking-widest">latest blog posts</div>
+            <h2 className="px-5 mb-3 font-medium uppercase tracking-widest">latest blog posts</h2>
             <LatestBlogPosts/>          
         </section>
         </>
--- src/components/indexComponents/recentExperienceContainer.js
+++ src/components/indexComponents/recentExperienceContainer.js
@@ -6,13 +6,12 @@ const Experience = () =>{
     return(
         <>
         <section className="latest-blog-container mt-20">            
-            <div className="px-5 mb-3 font-medium uppercase tracking-widest">Recent Experience</div>
+            <h2 className="px-5 mb-3 font-medium uppercase tracking-widest">Recent Experience</h2>
             <RecentExperience/>  
             <Link to="/about/#exp-list"  className="mb-3 not-sr-only md:mx-5 p-1 text-lg duration-150 transform hover:scale-105 mt-5" style={{border:'1px solid var(--color-gray700)',display:'block',width:'max-content'}}>Complete list of experience <BsArrowRight className="fill-current" style={{display:'inline-block',transform:'scale(1.3)',marginTop:'-4px'}}/></Link>          
         </section>
         </>
     );
-
 };
 
 export default Experience;
